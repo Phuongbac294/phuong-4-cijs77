b28a9c2ae95c64c0c4eed3b27201c030951f1889
Phuongbac294 <syphuong83@gmail.com>
2022-11-04 22:35:52 +0700

    up tối 4/11

--- cijs77-4/src/pages/pages6/AppWallet/appheader.js
+++ cijs77-4/src/pages/pages6/AppWallet/appheader.js
@@ -1,17 +1,19 @@
 import './appwallet.css';
+import { useState } from 'react';
 
 export default function AppHeader() {
-    const addNew = () => {
-
-    }
+    const [addTrue, setAddTrue] = useState(false)
+       
     return (
         <div className="appWallet__header">
+            {addTrue === false ?
             <button 
                 type='button' 
                 className='btn btn-violet btnappnew' 
-                onClick={addNew}>
-                    Add new expense
+                onClick={() => setAddTrue(true)} >
+                    Add new expense 
             </button>
+            :
             <from className='addNewExpense'>
                 <label for="text">Name</label>
                 <input type="text" name="text" placeholder='Enter name here' className='text1'></input><br></br>
@@ -19,9 +21,10 @@ export default function AppHeader() {
                 <input type="number" name="amount" placeholder='Enter amount here' className='amount'></input><br></br>
                 <label for="date">Date</label>
                 <input type="date" name="date" placeholder='Enter date here' className='date'></input><br></br>
-                <button type='button' className='btn btn-add'>Add</button>
-                <button type='button' className='btn btn-cancel'>Cancel</button>
+                <button type='submit' className='btn btn-add' >Add</button>
+                <button type='button' className='btn btn-cancel' onClick={() => setAddTrue(false)}>Cancel</button>
             </from>
+            }
         </div>
     );
 }
